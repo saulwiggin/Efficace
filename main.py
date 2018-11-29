102748865524c33705a9084024e30bd8874af9b6
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-11-29 11:59:09 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    return render_template(
+    'submitted_form.html',
+    name=name,
+    email=email,
+    site=site,
+    comments=comments)
